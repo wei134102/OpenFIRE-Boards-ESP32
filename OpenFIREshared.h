acb40a0b56f486c2927c323a2afa5262494ec9a6
jian wei <15667163+wei134103@user.noreply.gitee.com>
2026-08-02 13:06:12 +0800

    feat: 新增PAJ7025摄像头引脚与摇杆校准配置项
    - 新增cam_PAJ7025_MISO/MOSI/SCK/CS四个摄像头引脚枚举项
    - 修正原有摄像头引脚的描述文本，区分Wii摄像头和PAJ7025
    - 添加摇杆校准、死区、反转等一系列摇杆配置项

--- OpenFIREshared.h
+++ OpenFIREshared.h
@@ -97,6 +97,10 @@ public:
         analogX,
         analogY,
         tempPin,
+        cam_PAJ7025_MISO,
+        cam_PAJ7025_MOSI,
+        cam_PAJ7025_SCK,
+        cam_PAJ7025_CS,
         // Add non-button inputs here
         boardInputsCount
     } boardInputs_e;
@@ -129,13 +133,17 @@ public:
         {"RGB LED Green",       ledG            },
         {"RGB LED Blue",        ledB            },
         {"Wii Cam Clock",       wiiClockGen     },
-        {"Camera SDA",          camSDA          },
-        {"Camera SCL",          camSCL          },
+        {"Camera Wii Cam SDA",  camSDA          },
+        {"Camera Wii Cam SCL",  camSCL          },
         {"Peripherals SDA",     periphSDA       },
         {"Peripherals SCL",     periphSCL       },
         {"Analog Stick X",      analogX         },
         {"Analog Stick Y",      analogY         },
         {"Temperature Sensor",  tempPin         },
+        {"Camera PAJ7025 MISO", cam_PAJ7025_MISO},
+        {"Camera PAJ7025 MOSI", cam_PAJ7025_MOSI},
+        {"Camera PAJ7025 SCK",  cam_PAJ7025_SCK },
+        {"Camera PAJ7025 CS",   cam_PAJ7025_CS  },
     };
 
     // For Apps to use for lists of pin functions
@@ -201,7 +209,20 @@ public:
         tempWarning,
         tempShutdown,
         analogMode,
-        gunId,//add for display gunID
+        gunId,
+        analogDeadzone,
+        axisUnsigned,
+        analogCenterOffsetX,
+        analogCenterOffsetY,
+        analogCalMinX,
+        analogCalMaxX,
+        analogCalMinY,
+        analogCalMaxY,
+        analogCalValid,
+        analogInvertX,
+        analogInvertY,
+        analogKeysLayout,
+        analogSwapSticks,
         settingsTypesCount
     } settingsTypes_e;
 
